13855fa02485c2c34e0fb31e2abc31c3971667db
Ertugrul Sungur <ertugrullsngrr@gmail.com>
2023-12-01 19:11:22 +0300

    bug fixes

--- DSA.c
+++ DSA.c
@@ -6,8 +6,28 @@
 #define DSA_USED_SIZE(dsa) ((dsa->length) * (dsa->elementSize))
 #define DSA_INDEX_TO_P(dsa, index) (((char*)(dsa->data)) + ((dsa->elementSize) * (index)))
 
+#define DSA_ERR_INVALID_INDEX "Invalid index."
 
-void _dsa_memcpy(void *dest, const void *src, size_t size)
+
+int dsa_size_t_compare(const void *a, const void *b)
+{
+    const size_t _a = *(const size_t *)a;
+    const size_t _b = *(const size_t *)b;
+    if (_a < _b)
+    {
+        return -1;
+    }
+    if (_a > _b)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+void dsa_memcpy(void *dest, const void *src, size_t size)
 {
     char *d = dest;
     const char *s = src;
@@ -17,15 +37,70 @@ void _dsa_memcpy(void *dest, const void *src, size_t size)
     }
 }
 
-void _dsa_memcpy_reversed(void *dest, const void *src, size_t size)
+void dsa_memcpy_reverse(void *destEnd, const void *srcEnd, size_t size)
 {
-    char *d = dest;
-    const char *s = src;
+    char *d = destEnd;
+    const char *s = srcEnd;
     while (size--)
     {
         *d-- = *s--;
     }
 }
+
+int dsa_add_size_handle(DSA *dsa, int addSize)
+{
+    if (dsa->allocatedSize >= DSA_USED_SIZE(dsa)+addSize)
+    {
+        return 1;
+    }
+
+    size_t newSize = DSA_SIZE_COEFFICIENT * (DSA_USED_SIZE(dsa) + addSize);
+    void *temp = realloc(dsa->data, newSize);
+    if (temp == NULL)
+    {
+        perror("_dsa_add_size_handle: ");
+        return 0;
+    }
+    dsa->data = temp;
+    dsa->allocatedSize = newSize;
+    return 1;
+}
+
+int dsa_remove_size_handle(DSA *dsa, int removeSize){
+    if (dsa->allocatedSize <= DSA_SIZE_COEFFICIENT * (DSA_USED_SIZE(dsa) - removeSize)){
+        return 1;
+    }
+    size_t newSize = DSA_SIZE_COEFFICIENT * (DSA_USED_SIZE(dsa) - removeSize);
+    if (newSize == 0){
+        newSize = DSA_SIZE_COEFFICIENT * dsa->elementSize;
+    }
+    void *temp = realloc(dsa->data, newSize);
+    if (temp == NULL){
+        perror("_dsa_remove_size_handle: ");
+        return 0;
+    }
+    dsa->data = temp;
+    dsa->allocatedSize = newSize;
+    return 1;
+}
+
+void dsa_shift_block_right(DSA *dsa, size_t blockStartIndex, size_t blockEndIndex, size_t shiftCount)
+{
+    char *srcEnd = DSA_INDEX_TO_P(dsa, blockEndIndex) + (dsa->elementSize - 1);
+    char *destEnd = srcEnd + (dsa->elementSize * shiftCount);
+    size_t shiftedElementCount = blockEndIndex - blockStartIndex + 1;
+    dsa_memcpy_reverse(destEnd, srcEnd, shiftedElementCount * dsa->elementSize);
+}
+
+void dsa_shift_block_left(DSA *dsa, size_t blockStartIndex, size_t blockEndIndex, size_t shiftCount)
+{
+    char *src = DSA_INDEX_TO_P(dsa, blockStartIndex);
+    char *dest = src - (dsa->elementSize * shiftCount);
+    size_t shiftedElementCount = blockEndIndex - blockStartIndex + 1;
+    dsa_memcpy(dest, src, shiftedElementCount * dsa->elementSize);
+}
+
+
 
 DSA *dsa_create(size_t elementSize)
 {
@@ -51,68 +126,15 @@ DSA *dsa_create(size_t elementSize)
     return dsa;
 }
 
-int _dsa_add_size_handle(DSA *dsa, int addSize)
-{
-    if (dsa->allocatedSize >= DSA_USED_SIZE(dsa)+addSize)
-    {
-        return 1;
-    }
-    printf("add_size_handle_realloc\n");
-
-    size_t newSize = DSA_SIZE_COEFFICIENT * (DSA_USED_SIZE(dsa) + addSize);
-    void *temp = realloc(dsa->data, newSize);
-    if (temp == NULL)
-    {
-        perror("_dsa_add_size_handle: ");
-        return 0;
-    }
-    dsa->data = temp;
-    dsa->allocatedSize = newSize;
-    return 1;
-}
-
-int _dsa_remove_size_handle(DSA *dsa, int removeSize){
-    if (dsa->allocatedSize <= DSA_SIZE_COEFFICIENT * (DSA_USED_SIZE(dsa) - removeSize)){
-        return 1;
-    }
-    size_t newSize = DSA_SIZE_COEFFICIENT * (DSA_USED_SIZE(dsa) - removeSize);
-    void *temp = realloc(dsa->data, newSize);
-    if (temp == NULL){
-        perror("_dsa_remove_size_handle: ");
-        return 0;
-    }
-    dsa->data = temp;
-    dsa->allocatedSize = newSize;
-    return 1;
-}
-
-void _dsa_slide_left(DSA *dsa, size_t startIndex)
-{
-    // startIndex-1 will be overwrited
-    char *next = DSA_INDEX_TO_P(dsa, startIndex);
-    char *prev = next - dsa->elementSize;
-    size_t size = (dsa->length - startIndex) * dsa->elementSize;
-    _dsa_memcpy(prev, next, size);
-}
-
-void _dsa_slide_right(DSA *dsa, size_t startIndex)
-{
-    // startIndex will be empty
-    char *next = DSA_INDEX_TO_P(dsa, dsa->length);
-    char *prev = next-dsa->elementSize;
-    size_t size = (dsa->length-startIndex) * dsa->elementSize;
-    _dsa_memcpy_reversed(next, prev, size);
-}
-
 int dsa_add(DSA *dsa, const void *element)
 {
-    if (!_dsa_add_size_handle(dsa, dsa->elementSize))
+    if (!dsa_add_size_handle(dsa, dsa->elementSize))
     {
         printf("dsa_add: size could not handled.");
         return 0;
     }
     char *p = DSA_INDEX_TO_P(dsa, dsa->length);
-    _dsa_memcpy((void*)p, element, dsa->elementSize);
+    dsa_memcpy(p, element, dsa->elementSize);
     dsa->length++;
     return 1;
 }
@@ -121,14 +143,16 @@ int dsa_remove(DSA *dsa, size_t index)
 {
     if (index >= dsa->length)
     {
-        printf("dsa_remove: index can not be greater than length.\n");
+        printf(DSA_ERR_INVALID_INDEX);
         return 0;
     }
+
     if (index != dsa->length-1)
     {
-        _dsa_slide_left(dsa, index+1);
+        dsa_shift_block_left(dsa, index+1, dsa->length-1, 1);
     }
-    if(!_dsa_remove_size_handle(dsa, dsa->elementSize))
+    // TODO: Here dsa shifted left. But if size not handled, length couldn't decreased.
+    if(!dsa_remove_size_handle(dsa, dsa->elementSize))
     {
         printf("dsa_remove: size could not handled.\n");
         return 0;
@@ -141,37 +165,39 @@ int dsa_insert(DSA *dsa, size_t index, const void *element)
 {
     if (index >= dsa->length)
     {
-        printf("dsa_remove: index can not be greater than length.");
+        printf(DSA_ERR_INVALID_INDEX);
         return 0;
     }
     if (index == dsa->length-1 || dsa->length == 0)
     {
         return dsa_add(dsa, element);
     }
-    
-    _dsa_slide_right(dsa, index);
+    dsa_shift_block_right(dsa, index, dsa->length-1, 1);
     char *p = DSA_INDEX_TO_P(dsa, index);
-    _dsa_memcpy(p, element, dsa->elementSize);
+    dsa_memcpy(p, element, dsa->elementSize);
     dsa->length++;
+    return 1;
+}
+
+int dsa_clear(DSA *dsa)
+{
+    if (!dsa_remove_size_handle(dsa, DSA_USED_SIZE(dsa))){
+        printf("dsa_remove: size could not handled.\n");
+        return 0;
+    }
+    dsa->length = 0;
     return 1;
 }
 
 int dsa_add_multiple(DSA *dsa, const void *arr, size_t arrSize)
 {
-    if (!_dsa_add_size_handle(dsa, arrSize*dsa->elementSize))
+    if (!dsa_add_size_handle(dsa, arrSize*dsa->elementSize))
     {
         printf("dsa_add: size could not handled.");
         return 0;
     }
     char *p = DSA_INDEX_TO_P(dsa, dsa->length);
-    _dsa_memcpy((void*)p, arr, arrSize*dsa->elementSize);
+    dsa_memcpy(p, arr, arrSize*dsa->elementSize);
     dsa->length+=arrSize;
     return 1;
 }
-
-int dsa_remove_multiple(DSA *dsa, const size_t *indicesArr, size_t arrSize)
-{
-
-}
-
-
--- DSA.h
+++ DSA.h
@@ -14,5 +14,6 @@ DSA *dsa_create(size_t elementSize);
 int dsa_add(DSA *dsa, const void *element);
 int dsa_remove(DSA *dsa, size_t index);
 int dsa_insert(DSA *dsa, size_t index, const void *element);
+int dsa_clear(DSA *dsa);
 int dsa_add_multiple(DSA *dsa, const void *arr, size_t arrSize);
 #endif
